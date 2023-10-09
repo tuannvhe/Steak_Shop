5af2982c9bf30b7bf6d2df851fd5ea61ac4ab3c6
Nguyen Tuan <vnt28012000@gmail.com>
2023-10-09 17:11:46 +0700

    Config

--- SteakShop/Models/Steak_ShopContext.cs
+++ SteakShop/Models/Steak_ShopContext.cs
@@ -36,11 +36,11 @@ namespace SteakShop.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("server =(local); database = Steak_Shop;uid=sa;pwd=123;");
-            }
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            IConfigurationRoot configuration = builder.Build();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("Steak_Shop"));
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)

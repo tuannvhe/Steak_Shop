16629582fde390ee2d9566a820d4bca5bfdd38e4
đức anh trần <tdanhaltt18@gmail.com>
2023-10-20 13:10:54 +0700

    booking table

---
+++ SteakShop/Controllers/BookTableController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SteakShop.Models;
+
+namespace SteakShop.Controllers
+{
+    public class BookTableController : Controller
+    {
+        private readonly Steak_ShopContext _context;
+
+        public BookTableController(Steak_ShopContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult BookTable()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GetInfo(BookTable model)
+        {
+            return View("~Views/Food/Food.cshtml");
+        }
+    }
+}

1fb868e41269c714d6363f4b0d42244ecdbb126c
đức anh trần <tdanhaltt18@gmail.com>
2023-10-22 23:26:53 +0700

    thêm giỏ hàng

---
+++ SteakShop/Controllers/CartController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SteakShop.Controllers
+{
+    public class CartController : Controller
+    {
+        [HttpPost]
+        public IActionResult Cart()
+        {
+            return View();
+        }
+    }
+}

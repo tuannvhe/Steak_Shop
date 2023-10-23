d4c38bcca984c8cf1330dc8694b5312497f73a31
đức anh trần <tdanhaltt18@gmail.com>
2023-10-23 20:25:51 +0700

    thêm check-out

---
+++ SteakShop/Controllers/CheckOutController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SteakShop.Models;
+
+namespace SteakShop.Controllers
+{
+    public class CheckOutController : Controller
+    {
+        private readonly Steak_ShopContext _context;
+
+        public CheckOutController(Steak_ShopContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+    }
+}
